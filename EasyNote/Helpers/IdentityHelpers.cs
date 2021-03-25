c36e9eb8ea366c04c0fb8819493ad558f6fca45d
Bircan Ataş <atas.bircan@hotmail.com>
2021-03-25 13:22:18 +0300

    Notes ui

---
+++ EasyNote/Helpers/IdentityHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace EasyNote.Helpers
+{
+    public static class IdentityHelpers
+    {
+        public static string GetDisplayName(this IIdentity identity)
+        {
+
+            //https://stackoverflow.com/a/21405165/8783782
+            return ((ClaimsIdentity)identity).FindFirst("DisplayName")?.Value;
+        }
+    }
+}
